0b633c08f38630dad62c499b851cb0a6d18966d7
GloRosado07/wdd230 <address>
2024-02-20 22:20:45 -0600

    database update

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
 const express = require('express');
+
+const mongodb = require('./data/database');
 const app = express();
 
 const port = process.env.PORT || 3000;
 
 app.use('/', require('./routes'));
 
-app.listen(process.env.PORT || port, () => {
-    console.log('Web Server is listening at port ' + (process.env.PORT || port));
-  });
+// app.listen(process.env.PORT || port, () => {
+//     console.log('Web Server is listening at port ' + (process.env.PORT || port));
+//   });
+
+mongodb.initDb((err) => {
+    if (err) {
+        console.log(err);
+    } else {
+        app.listen(port, () => {console.log(`Database is listening and running on port ${port}`)});
+    }
+});
